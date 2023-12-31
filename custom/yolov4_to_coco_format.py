4e60f915a62782091b12ceafc9d1b22ae046b7a3
zli <zli@mail.com>
2023-12-31 21:27:25 +0100

    yolo format conversion

--- custom/yolov4_to_coco_format.py
+++ custom/yolov4_to_coco_format.py
@@ -5,6 +5,8 @@ from fiftyone import ViewField
 name = "coco_person"
 dataset_dir = "/home/zli/data/person_dataset"
 
+# rm -rf ~/.fiftyone/
+
 # Create the dataset
 dataset = fo.Dataset.from_dir(
     dataset_dir=dataset_dir,
