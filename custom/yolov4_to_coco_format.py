f7c5d816241e8a3f87f50a1bfd3bb39adee51908
zli <zli@mail.com>
2024-02-19 14:25:52 +0100

    yolov4-to-coco update

--- custom/yolov4_to_coco_format.py
+++ custom/yolov4_to_coco_format.py
@@ -1,19 +1,33 @@
 import fiftyone as fo
-import fiftyone.zoo as foz
-from fiftyone import ViewField
+import os.path as osp
 
-name = "coco_person"
-dataset_dir = "/home/zli/data/person_dataset"
+def main():
+    """Convert a YOLOv4 dataset into COCO dataset format.
+    The file structure is
+    <dataset_dir>/
+    ├── obj.names
+    ├── data/
+    │   ├── <uuid1>.jpg
+    │   ├── <uuid1>.txt
+    │   ├── <uuid2>.jpg
+    │   ├── <uuid2>.txt
+    where `obj.names` contains one line: 'person'
+    """
+    dataset_dir = osp.expanduser('~/data/person_detection_data')
 
-# rm -rf ~/.fiftyone/
+    # The cached fiftyone files may cause error 100
+    # rm -rf ~/.fiftyone/
 
-# Create the dataset
-dataset = fo.Dataset.from_dir(
-    dataset_dir=dataset_dir,
-    dataset_type=fo.types.YOLOv4Dataset,
-    name=name,
-)
-# session = fo.launch_app(dataset)
-# input('Press Enter to continue...')
+    # Create the dataset
+    dataset = fo.Dataset.from_dir(
+        dataset_dir=dataset_dir,
+        dataset_type=fo.types.YOLOv4Dataset,
+        name='coco_person',
+    )
+    # session = fo.launch_app(dataset)
+    # input('Press Enter to continue...')
 
-dataset.export(export_dir='/home/zli/data/coco_person', dataset_type=fo.types.COCODetectionDataset)
+    dataset.export(export_dir=osp.expanduser('~/data/milestone_coco_person'), dataset_type=fo.types.COCODetectionDataset)
+
+if __name__ == '__main__':
+    main()
